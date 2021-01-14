c4f6f4762c60ca69590b3d52dd950060f29c024f
Andrew Chau <andrewchau333@gmail.com>
2021-01-13 19:29:10 -0800

    Making header responsive

--- src/components/core/Header/index.jsx
+++ src/components/core/Header/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import {Navbar, Nav, Button} from 'react-bootstrap'
+import { Navbar, Nav, Button } from 'react-bootstrap'
 import Logo from '../../../assets/amp-logo-header.png'
 
 
 const Header = () => {
-    return(
-        <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">
-          <img src={Logo} alt="Logo" height="60" />
-        </Navbar.Brand>
+  return (
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Navbar.Brand href="/">
+        <img src={Logo} alt="Logo" height="60" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="/campuses">CAMPUSES</Nav.Link>
-          <Nav.Link href="/mentors">COLLEGE MENTORS</Nav.Link>
+          <Nav.Link href="/mentors">MENTORS</Nav.Link>
           <Nav.Link href="/advisors">ADVISORS</Nav.Link>
           <Nav.Link href="/program">PROGRAM</Nav.Link>
           <Button href="/apply">APPLY NOW</Button>
         </Nav>
-      </Navbar>
-    )
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
 export default Header;
