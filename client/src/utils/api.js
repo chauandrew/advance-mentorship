8d5cba2db1b6e8ae6f3224be2ead2218e0debc68
Andrew Chau <andrewchau333@gmail.com>
2021-01-18 15:49:20 -0800

    user signup typechecking, frontend api call

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -19,5 +19,23 @@ export default {
     // Get advisors by the campus anchor tag (eg apu, american, ucla)
     getAdvisorsByCampus: async function (campusAnchor) {
         return (await axios.get(`${prefix}/api/v1/advisor/campus/${campusAnchor}`)).data
+    },
+    /**
+     * Submit a user's signup info
+     * @param {Object} userInfo contains: firstname*, lastname*, email**, gender*, 
+     * school*, otherSchool, city*, collegeTrack, careerTrack, adultingTrack, 
+     * leadershipTrack, gradSchoolTrack, softSkillsTrack.
+     * (*required, **must be edu email)
+     */
+    userSignup: async function (userInfo) {
+        if (!userInfo || typeof(userInfo) !== "object") {
+            return Error("Invalid userInfo!")
+        }
+        if (!('firstname' in userInfo) || !('lastname' in userInfo) ||
+            !('email' in userInfo) || !('gender' in userInfo) ||
+            !('school' in userInfo) || !('city' in userInfo)) {
+            return Error("userInfo must contain firstname, lastname, email, gender, school, city")
+        }
+        return (await axios.post(`${prefix}/api/v1/signup`, userInfo))
     }
 }
--- server/api/routes/SignupRoutes.js
+++ server/api/routes/SignupRoutes.js
@@ -6,6 +6,12 @@ const gsheetService = require('../../services/GsheetService');
  * Insert a row for new mentorship signups
  */
 router.post('/', (req, res) => {
+    if (!('firstname' in req.body) || !('lastname' in req.body) ||
+        !('email' in req.body) || !('gender' in req.body) ||
+        !('school' in req.body) || !('city' in req.body)) {
+        res.status(400)
+        res.send("Body must contain firstname, lastname, email, gender, school, city")
+    }
     const row = [
         req.body['firstname'],
         req.body['lastname'],
@@ -14,12 +20,12 @@ router.post('/', (req, res) => {
         req.body['school'],
         req.body['otherSchool'],
         req.body['city'],
-        req.body['collegeTrack'],
-        req.body['careerTrack'],
-        req.body['adultingTrack'],
-        req.body['leadershipTrack'],
-        req.body['gradSchoolTrack'],
-        req.body['softSkillsTrack']
+        req.body['collegeTrack'] ? true : false,
+        req.body['careerTrack'] ? true : false,
+        req.body['adultingTrack'] ? true : false,
+        req.body['leadershipTrack'] ? true : false,
+        req.body['gradSchoolTrack'] ? true : false,
+        req.body['softSkillsTrack'] ? true : false
     ]
     gsheetService.appendRows('signups', [row])
         .then(rows => res.json(rows))
